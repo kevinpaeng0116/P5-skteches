fa539c365d03660a236e94c715d78f1adaf1b779
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-03 13:02:54 -0400

    Sketch 1. Blue S

---
+++ s-sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(1000, 1000);
+}
+
+function draw() {
+	fill(0, 0, 255);
+  rect(50, 100, 100, 10);
+  rect(50, 100, 10, 100);
+  rect(50, 200, 100, 10);
+  rect(150, 200, 10, 100);
+  rect(50, 290, 100, 10);
+}
