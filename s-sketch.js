5ce57a8c5f7d29b53f5a4cf878ffaa0afa3df659
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-03 13:26:53 -0400

    Second S Sketch. Spiderman

--- s-sketch.js
+++ s-sketch.js
@@ -3,10 +3,49 @@ function setup() {
 }
 
 function draw() {
+	//Top of S
+	drawLine(10);
+
+	//First Side of S(left)
+  	fill(255, 0, 0);
+  	square(10, 35, 25);
+
+  	fill(0, 0, 255);
+  	
+  	square(10, 60, 25);
+  	fill(255, 0, 0);
+  	square(10, 85, 25);
+
+  	//Middle line of S
+  	drawLine(110);
+
+  	//Second Side of S(right)
+  	fill(255, 0, 0);
+  	square(110, 135, 25);
+
+  	fill(0, 0, 255);
+  	square(110, 160, 25);
+
+  	fill(255, 0, 0);
+  	square(110, 185, 25);
+
+  	//draw bottom line
+  	drawLine(210);
+}
+
+function drawLine(yValue){
 	fill(0, 0, 255);
-  rect(50, 100, 100, 10);
-  rect(50, 100, 10, 100);
-  rect(50, 200, 100, 10);
-  rect(150, 200, 10, 100);
-  rect(50, 290, 100, 10);
+  	square(10,yValue,25);
+
+	fill(255, 0, 0);
+  	square(35,yValue,25);
+
+	fill(0, 0, 255);
+  	square(60, yValue, 25);
+
+	fill(255, 0, 0);
+  	square(85,yValue,25);
+
+	fill(0, 0, 255);
+  	square(110, yValue, 25);
 }
