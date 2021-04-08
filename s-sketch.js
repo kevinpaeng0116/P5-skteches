21e25f9066b5c792b6ebaf3d1e9f3cf9b62651f8
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-08 12:31:26 -0400

    sketch 5
    multi shape S. circles, arc and lines

--- s-sketch.js
+++ s-sketch.js
@@ -4,10 +4,24 @@ function setup() {
 
 function draw() {
 	//top S
-	drawS(10, 10, color(255,0,0), color(0,0,252));
+	//drawS(10, 10, color(255,0,0), color(0,0,252));
 
   	//Bottom S
-  	drawS(40, 40, color(0,255,0), color(100,100,20));
+  	//drawS(40, 40, color(0,255,0), color(100,100,20));
+  	//noStroke();
+  	strokeWeight(0)
+	fill(0,0,0);
+  	arc(400, 300, 280, 280, PI, TWO_PI);
+  	arc(400, 400, 280, 280, TWO_PI, PI);
+
+  	fill(255,255,255);
+  	//circle(300, 150, 40);
+  	circle(400, 300, 140);
+  	circle(400, 400, 140);
+
+  	fill(0,0,0);
+  	strokeWeight(50);
+  	line(280, 330, 520, 370);
 }
 
 function drawS(xValue, yValue, color1, color2){
