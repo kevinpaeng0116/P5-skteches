59595a9b0130d17b76965f2a09acedb982f3f8e5
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-11 19:35:24 -0400

    remade spiderman

--- s-sketch.js
+++ s-sketch.js
@@ -4,70 +4,56 @@ function setup() {
 
 function draw() {
 
-	drawS(300, 300, color(255,0,0), color(0,255,0), color(0,0,255));
+	drawS(300, 300, color(255,0,0), color(0,0,255));
 
-	fill(255,0,0,63);
-	rect(250, 250, 220, 50);
-
-	fill(0,255,0,63);
-	rect(250, 310, 220, 50);
-
-	fill(0,0,255,63);
-	rect(250, 370, 220, 50);
-
-	fill(255,0,0,63);
-	rect(250, 430, 220, 50);
-
-	fill(0,255,0,63);
-	rect(250, 490, 220, 50);
 
 }
 
-function drawS(xValue, yValue, color1, color2, color3){
+function drawS(xValue, yValue, color1, color2){
 	const ogX = xValue;
 	//Top of S
-	drawLine(ogX, yValue, color1, color2, color3);
+	drawLine(ogX, yValue, color1, color2);
 
 	//First Side of S(left)
-  	fill(color1);
+  	fill(color2);
   	square(xValue, yValue+=25, 25);
 
-  	fill(color2);  	
+  	fill(color1);  	
   	square(xValue, yValue += 25, 25);
   	
-  	fill(color3);
+  	fill(color2);
   	square(xValue, yValue += 25, 25);
 
   	//Middle line of S
-  	drawLine(ogX, yValue += 25, color1, color2, color3);
+  	drawLine(ogX, yValue += 25, color1, color2);
 
   	//Second Side of S(right)
-  	fill(color1);
+  	fill(color2);
   	square(xValue+=100, yValue+=25, 25);
 
-  	fill(color2);
+  	fill(color1);
   	square(xValue, yValue += 25, 25);
  	
-	  fill(color3)
+	  fill(color2)
   	square(xValue, yValue+=25, 25);
 
   	//draw bottom line
-  	drawLine(ogX, yValue+=25, color1, color2, color3);
+  	drawLine(ogX, yValue+=25, color1, color2);
 }
 
-function drawLine(xValue, yValue, color1, color2, color3){
+function drawLine(xValue, yValue, color1, color2){
 	fill(color1);
   	square(xValue, yValue,25);
 
 	fill(color2);
   	square(xValue += 25,yValue,25);
 
-	fill(color3);
+	fill(color1);
   	square(xValue += 25, yValue, 25);
 
-	fill(color1);
+	fill(color2);
   	square(xValue += 25,yValue,25);
 
-	fill(color2);
+	fill(color1);
   	square(xValue += 25, yValue, 25);
 }
