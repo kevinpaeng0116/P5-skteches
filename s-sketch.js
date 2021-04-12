fb10bbe643b52a14ee4f7dc94ab1b4176ff7d4fe
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-11 20:06:26 -0400

    gradients S

--- s-sketch.js
+++ s-sketch.js
@@ -1,13 +1,33 @@
+let b1, b2, c1, c2;
+
 function setup() {
   createCanvas(1000, 1000);
+  
+  b1 = color(255);
+  b2 = color(0);
+  c1=color(204, 102, 0);
+  c2 = color(0, 102, 153);
+
+  noLoop();
 }
 
 function draw() {
 
-	drawS(300, 300, color(255,0,0), color(0,0,255));
+  setGradient(0, 0, 300, 300, c1, c2);
 
-
+  drawS(90, 30, color(12, 121, 34), color(167, 121, 89))
 }
+
+function setGradient(x,y,w,h,c1,c2){
+  for(let i = x; i <= x + w; i++){
+    let inner = map(i,x,x+w,0,1);
+    let c = lerpColor(c1, c2, inner);
+    stroke(c);
+    line(i,y,i, y+h);
+  }
+  
+}
+
 
 function drawS(xValue, yValue, color1, color2){
 	const ogX = xValue;
