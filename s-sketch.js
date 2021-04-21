bdc06c112e9c6933faff22caf4639308dac1d043
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-21 19:47:05 -0400

    Superman s

--- s-sketch.js
+++ s-sketch.js
@@ -1,40 +1,39 @@
-
-let b1, b2, c1, c2;
-
 function setup() {
   createCanvas(1000, 1000);
-  b1 = color(255);
-  b2 = color(0);
-  c1=color(204, 102, 100);//left
-  c2 = color(0, 102, 153);//right
-
-  noLoop();
 }
 
-function draw() {
-  setGradient(0, 0, 1000, 1000, c1, c2);
-  quad(330, 100, 430, 100, 300, 300, 200, 300);
+function draw() { 
+  fill(20, 20, 255);
+  square(0,0,1000);
+  fill(255, 255, 0);
+  square(300, 300, 300);
+  textSize(200);
+  fill(255,20, 20);
+  text("S", 380,520)
 
-  quad(200, 330, 300, 330, 430, 530, 330, 530);
+  noStroke();
+  fill(0,0,0);
+  rect(350,300, 250, 72);
+  fill(20, 20, 255);
+  triangle(450,300, 600, 300, 600, 450);
+  triangle(600, 370, 600, 600, 450, 600);
+  triangle(300, 370, 300, 600, 450, 600);
+  triangle(450, 300, 300, 300, 300, 450);
+  rect(350,300, 250, 70);
 
-  quad(330, 560, 430, 560, 300, 730, 200, 730);
 
-  fill(c1 );
-  quad(600, 100, 700, 100, 800, 300, 700, 300);
-  quad(700, 330, 800, 330, 700, 530, 600, 530);
-  quad(600, 560, 700, 560, 800, 730, 700, 730);
+var x = 1;
+for (var i = 50; i > 0; i--) {
+  fill(255, 255, 255, i)
+  ellipse(mouseX, mouseY, x, x);
+  x+= 2;
 }
 
-function setGradient(x,y,w,h,c1,c2){
-  for(let i = x; i <= x + w; i++){
-    let inner = map(i,x,x+w,0,1);
-    let c = lerpColor(c1, c2, inner);
-    stroke(c);
-    fill(c);
-    line(i,y,i, y+h);
-  }
-  
+ //
+  //
+
 }
+
 
 
 
