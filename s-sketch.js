d933f33d43c72c7f71b1b02033a8b9b9cfc817e4
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-20 21:53:38 -0400

    Update S sketch
    update s

--- s-sketch.js
+++ s-sketch.js
@@ -11,16 +11,23 @@ function preload(){
 
 function draw() {
   image(img, 0, 0);
-  fill(205,211,211)
-  textSize(700);
+  textSize(50);
 
-  text("S",250,800);
-
-  
+  text("S",10,60);
   for(let x = 0; x < 1000; x+=10){
     for(let y = 0; y < 1000; y+=10){
       fill(get(x,y));
       circle(x,y,8);
+
+        fill(0,0,0);
+    strokeWeight(50);
+    line(280, 330, 520, 370);
+    strokeCap(SQUARE)
+    line(350, 300, 460, 270);
+    line(350, 300, 300, 450);
+    line(300, 450, 430, 410);
+    line(430, 410, 400, 530);
+    line(270, 570, 400, 530);
     }
   }
 }
