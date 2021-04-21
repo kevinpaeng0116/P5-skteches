10861846fb8080e95766416e29b8a88ef8ee43f5
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-20 22:29:11 -0400

    pattern for loop S

--- s-sketch.js
+++ s-sketch.js
@@ -1,51 +1,26 @@
-let img;
 function setup() {
   createCanvas(1000, 1000);
-  img = loadImage('./635426.jpg');
-
-}
-
-function preload(){
-  
 }
 
 function draw() {
-  image(img, 0, 0);
-  textSize(50);
+fill(25, 25, 25);
+for(var x = 0; x < 1000; x+=50){
+  for (var i = 0; i < 1000; i+= 50) {
+    let rand = random(1, 50);
+    square(i, x, rand);
+   }
+ }
 
-  text("S",10,60);
-  for(let x = 0; x < 1000; x+=10){
-    for(let y = 0; y < 1000; y+=10){
-      fill(get(x,y));
-      circle(x,y,8);
 
-        fill(0,0,0);
-    strokeWeight(50);
-    line(280, 330, 520, 370);
-    strokeCap(SQUARE)
-    line(350, 300, 460, 270);
-    line(350, 300, 300, 450);
-    line(300, 450, 430, 410);
-    line(430, 410, 400, 530);
-    line(270, 570, 400, 530);
-    }
-  }
+push();
+stroke(0);
+fill(100,100,100);
+ rect(300, 300, 350, 100);
+ rect(300, 300, 100, 200);
+ rect(300, 500, 350,100);
+ rect(550, 600, 100, 200);
+  rect(300,700, 350, 100)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
