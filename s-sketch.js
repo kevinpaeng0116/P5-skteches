d34046dae6673fa5ba0f304febce50246884044e
Kevin Paeng <kevinpaeng@gmail.com>
2021-04-20 23:01:47 -0400

    upgraded line S

--- s-sketch.js
+++ s-sketch.js
@@ -3,20 +3,50 @@ function setup() {
 }
 
 function draw() {
-stroke(3);
+strokeWeight(1);
 
+push()
+strokeWeight(2);
 line(0,0, 700, 300);
+
+pop()
 line(0,0, 670, 270);
+
+push()
+strokeWeight(5)
+
+pop()
 line(0,0, 610, 240);
+
+push();
+strokeWeight(4)
 line(0,0, 560, 260);
+
+pop()
 line(0,0, 570, 290);
+
 line(0,0, 600, 310);
+
+push()
+strokeWeight(2)
 line(0,0, 670, 360);
+
 line(0,0, 700, 400);
+
+pop()
 line(0,0, 750, 500);
+
+push()
+strokeWeight(5)
 line(0,0, 720, 520);
+
+pop()
 line(0,0, 640, 480);
+
+push()
+strokeWeight(8)
 line(0,0, 550, 430);
+
 }
 
 
